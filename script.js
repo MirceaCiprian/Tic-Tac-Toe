4186aa32efb0939b4f44acbb55b7eb487339c857
MirceaCiprian <121874251+MirceaCiprian@users.noreply.github.com>
2023-01-28 13:40:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let player = 1;
+let gameEnd = 0;
+let elem = [2, 2, 2 ,2, 2, 2, 2, 2, 2];
+let countClicks = 0
+
+function Game(value) {
+    if(!gameEnd) {
+        countClicks++;
+        if(player === 1)
+        {
+            document.getElementById(value).innerHTML = 'X';
+            document.getElementById(value).onclick = null;
+            player = 0;
+            elem[value - 1] = 1; 
+            CheckWinner();
+        }
+        else
+        {
+            document.getElementById(value).innerHTML = '0';
+            document.getElementById(value).onclick = null;
+            player = 1;
+            elem[value - 1] = 0; 
+            CheckWinner();
+        }
+    }
+}
+
+function CheckWinner() {
+    if(areEqual(elem[0],elem[1], elem[2], 1) || 
+    areEqual(elem[3],elem[4], elem[5], 1) || 
+    areEqual(elem[6],elem[7], elem[8], 1) ||
+    areEqual(elem[0],elem[3], elem[6], 1) ||
+    areEqual(elem[1],elem[4], elem[7], 1) ||
+    areEqual(elem[2],elem[5], elem[8], 1) ||
+    areEqual(elem[0],elem[4], elem[8], 1) ||
+    areEqual(elem[2],elem[4], elem[6], 1))
+    { 
+        document.getElementById("top").innerHTML = "Player 1 Wins!";
+        document.getElementById("topbutton").innerHTML = "RestartGame";
+        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+        gameEnd = 1;
+    }
+
+    if(areEqual(elem[0],elem[1], elem[2], 0) || 
+    areEqual(elem[3],elem[4], elem[5], 0) || 
+    areEqual(elem[6],elem[7], elem[8], 0) ||
+    areEqual(elem[0],elem[3], elem[6], 0) ||
+    areEqual(elem[1],elem[4], elem[7], 0) ||
+    areEqual(elem[2],elem[5], elem[8], 0) ||
+    areEqual(elem[0],elem[4], elem[8], 0) ||
+    areEqual(elem[2],elem[4], elem[6], 0))
+    { 
+        document.getElementById("top").innerHTML = "Player 0 Wins!";
+        document.getElementById("topbutton").innerHTML = "RestartGame";
+        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+        gameEnd = 1;
+    }
+    if(countClicks == 9 && gameEnd == 0)
+    {
+        document.getElementById("top").innerHTML = "Its a draw!";
+        document.getElementById("topbutton").innerHTML = "RestartGame";
+        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+    }
+}
+
+function areEqual(elem1, elem2, elem3, value) {
+    if(value === elem1 && elem1 === elem2 && elem2 === elem3)
+        return true;
+    return false;
+}
+
+function RestartGame()
+{
+    window.location.reload();
+}
