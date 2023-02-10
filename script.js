b023471d78ce83c7a2da5faf79dea39396bd29a1
Crydown <41234084+Crydown@users.noreply.github.com>
2023-02-10 12:43:43 +0200

    functions name updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let gameEnd = 0;
 let elem = [2, 2, 2 ,2, 2, 2, 2, 2, 2];
 let countClicks = 0
 
-function Game(value) {
+function game(value) {
     if(!gameEnd) {
         countClicks++;
         if(player === 1)
@@ -12,7 +12,7 @@ function Game(value) {
             document.getElementById(value).onclick = null;
             player = 0;
             elem[value - 1] = 1; 
-            CheckWinner();
+            checkWinner();
         }
         else
         {
@@ -20,12 +20,12 @@ function Game(value) {
             document.getElementById(value).onclick = null;
             player = 1;
             elem[value - 1] = 0; 
-            CheckWinner();
+            checkWinner();
         }
     }
 }
 
-function CheckWinner() {
+function checkWinner() {
     if(areEqual(elem[0],elem[1], elem[2], 1) || 
     areEqual(elem[3],elem[4], elem[5], 1) || 
     areEqual(elem[6],elem[7], elem[8], 1) ||
@@ -37,7 +37,7 @@ function CheckWinner() {
     { 
         document.getElementById("top").innerHTML = "Player 1 Wins!";
         document.getElementById("topbutton").innerHTML = "RestartGame";
-        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+        document.getElementById("topbutton").setAttribute('onclick','restartGame()');
         gameEnd = 1;
     }
 
@@ -52,14 +52,14 @@ function CheckWinner() {
     { 
         document.getElementById("top").innerHTML = "Player 0 Wins!";
         document.getElementById("topbutton").innerHTML = "RestartGame";
-        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+        document.getElementById("topbutton").setAttribute('onclick','restartGame()');
         gameEnd = 1;
     }
     if(countClicks == 9 && gameEnd == 0)
     {
         document.getElementById("top").innerHTML = "Its a draw!";
         document.getElementById("topbutton").innerHTML = "RestartGame";
-        document.getElementById("topbutton").setAttribute('onclick','RestartGame()');
+        document.getElementById("topbutton").setAttribute('onclick','restartGame()');
     }
 }
 
@@ -69,7 +69,7 @@ function areEqual(elem1, elem2, elem3, value) {
     return false;
 }
 
-function RestartGame()
+function restartGame()
 {
     window.location.reload();
 }
