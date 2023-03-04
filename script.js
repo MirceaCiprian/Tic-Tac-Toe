c27ef663450b9f570cb4155a5f18012861523406
Crydown <41234084+Crydown@users.noreply.github.com>
2023-03-04 14:58:42 +0200

    duplicate code removed

--- script.js
+++ script.js
@@ -6,55 +6,37 @@ let countClicks = 0
 function game(value) {
     if(!gameEnd) {
         countClicks++;
-        if(player === 1)
-        {
+        if(player === 1) {
             document.getElementById(value).innerHTML = 'X';
-            document.getElementById(value).onclick = null;
-            player = 0;
-            elem[value - 1] = 1; 
-            checkWinner();
         }
-        else
-        {
+        else {
             document.getElementById(value).innerHTML = '0';
-            document.getElementById(value).onclick = null;
-            player = 1;
-            elem[value - 1] = 0; 
-            checkWinner();
         }
+        document.getElementById(value).onclick = null;
+        elem[value - 1] = player; 
+        checkWinner(player);
+        player = 1 - player;
+        checkDraw();
     }
 }
 
-function checkWinner() {
-    if(areEqual(elem[0],elem[1], elem[2], 1) || 
-    areEqual(elem[3],elem[4], elem[5], 1) || 
-    areEqual(elem[6],elem[7], elem[8], 1) ||
-    areEqual(elem[0],elem[3], elem[6], 1) ||
-    areEqual(elem[1],elem[4], elem[7], 1) ||
-    areEqual(elem[2],elem[5], elem[8], 1) ||
-    areEqual(elem[0],elem[4], elem[8], 1) ||
-    areEqual(elem[2],elem[4], elem[6], 1))
-    { 
-        document.getElementById("top").innerHTML = "Player 1 Wins!";
+function checkWinner(user) {
+    if(areEqual(elem[0],elem[1], elem[2], user) || 
+    areEqual(elem[3],elem[4], elem[5], user) || 
+    areEqual(elem[6],elem[7], elem[8], user) ||
+    areEqual(elem[0],elem[3], elem[6], user) ||
+    areEqual(elem[1],elem[4], elem[7], user) ||
+    areEqual(elem[2],elem[5], elem[8], user) ||
+    areEqual(elem[0],elem[4], elem[8], user) ||
+    areEqual(elem[2],elem[4], elem[6], user)) { 
+        document.getElementById("top").innerHTML = "Player " + user + " Wins!";
         document.getElementById("topbutton").innerHTML = "RestartGame";
         document.getElementById("topbutton").setAttribute('onclick','restartGame()');
         gameEnd = 1;
     }
+}
 
-    if(areEqual(elem[0],elem[1], elem[2], 0) || 
-    areEqual(elem[3],elem[4], elem[5], 0) || 
-    areEqual(elem[6],elem[7], elem[8], 0) ||
-    areEqual(elem[0],elem[3], elem[6], 0) ||
-    areEqual(elem[1],elem[4], elem[7], 0) ||
-    areEqual(elem[2],elem[5], elem[8], 0) ||
-    areEqual(elem[0],elem[4], elem[8], 0) ||
-    areEqual(elem[2],elem[4], elem[6], 0))
-    { 
-        document.getElementById("top").innerHTML = "Player 0 Wins!";
-        document.getElementById("topbutton").innerHTML = "RestartGame";
-        document.getElementById("topbutton").setAttribute('onclick','restartGame()');
-        gameEnd = 1;
-    }
+function checkDraw() {
     if(countClicks == 9 && gameEnd == 0)
     {
         document.getElementById("top").innerHTML = "Its a draw!";
@@ -69,7 +51,6 @@ function areEqual(elem1, elem2, elem3, value) {
     return false;
 }
 
-function restartGame()
-{
+function restartGame() {
     window.location.reload();
 }
